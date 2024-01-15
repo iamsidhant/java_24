98b760671cc81e123596a1308bbec76697f4ca03
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-01-15 15:05:31 +0000

    data types

---
+++ dataTypes.java
@@ -0,0 +1,15 @@
+class Hello{
+    public static void main(String a[]){
+        int num1 = 9;
+        byte by = 127;
+        short sh = 586;
+        long l = 8567l;
+
+        float f = 5.8f;
+        double d = 5.6;
+
+        char c = 'k';
+
+        boolean b = true;
+    }
+}
