13ac276dbc2bc56dad52682d3a29dd02346576a8
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-01-15 10:54:09 +0000

    variables

---
+++ variables.java
@@ -0,0 +1,8 @@
+class Hello{
+    public static void main(String a[]){
+        int num1 = 15;
+        int num2 = 3;
+        int result = num1 + num2;
+        System.out.println(result);
+    }
+}
