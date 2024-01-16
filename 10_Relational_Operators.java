993cbd2d0eaef9a144298c9929ecb15826c77ae9
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-01-16 15:49:33 +0000

    relational Operators

---
+++ 10_Relational_Operators.java
@@ -0,0 +1,22 @@
+class Hello{
+    public static void main(String args[]){
+        int x = 6;
+        int y = 5;
+        // boolean result = x > y;
+        // boolean result = x < y;
+        // boolean result = x <= y;
+        // boolean result = x >= y;
+        // boolean result = x == y;
+        // boolean result = x != y;
+        // System.out.println(result);
+
+        double a = 9.6;
+        double b = 9.9;
+
+        boolean res = a <= b;
+        // boolean res = a >= b;
+
+        System.out.println(res);
+    }
+
+}
