8831ed6830b99cd58b31f657871831b8e71310e7
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-01-17 06:32:30 +0000

    Ternary Operators

---
+++ 14_Ternary_Operators.java
@@ -0,0 +1,19 @@
+
+class Hello{
+    public static void main(String args[]){
+        // int n = 9;
+        // int result = 0;
+
+        // if(n % 2 == 0)
+        //     result = 10;
+        // else
+        //     result = 20;
+        // System.out.println(result);
+
+        int n = 653;
+        int result = 0;
+        result = n % 2 == 0 ? 10 : 20;
+        System.out.println(result);
+
+    }
+}
