6b34877570fcf76fd2fe5e1f0f5358aa1a205e22
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-01-17 10:04:11 +0000

    loops

--- 16_Loop.java
+++ 16_Loop.java
@@ -1,8 +1,11 @@
+
 class Hello{
     public static void main(String args[])
     {
         System.out.println("Hii");
-        // repeat the above statement multiple times
-        // loop - for, while, do while
+        /*   Need of a Loop  */
+        
+            // to repeat the above statement multiple times
+            // loop - for, while, do while
     }
 }
---
+++ 18_do_While_Loop.java
@@ -0,0 +1,12 @@
+
+class Hello{
+    public static void main(String args[]){
+        int i = 1;
+        do
+        {
+            System.out.println("Hi " + i);
+            i++;
+        }
+        while(i <= 4);
+    }
+}
