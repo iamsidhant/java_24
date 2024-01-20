51233706cb1e444c10e644ad77a933a06ddab654
Sidhant Arya <121373417+iamsidhant@users.noreply.github.com>
2024-01-20 07:29:54 +0000

    Array

---
+++ 31_drawbacks_of_Array.java
@@ -0,0 +1,14 @@
+class Demo{
+    public static void main(String[] args)
+    {
+        int nums = new int[4];
+    }
+}
+
+
+//      Drawbacks :-
+
+// - Memory Allocation is continuous.
+// - The size of array is fixed. Array size cannot be expand.
+// - Searching takes time.
+// - Array can store values of only same type. It can store homogeneous type value only.
